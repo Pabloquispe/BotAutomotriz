2afc2dc4cd5faf68dba193429a0f9c200dc9bfc3
Pabloquispe <131137809+Pabloquispe@users.noreply.github.com>
2024-07-02 23:19:01 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,3 @@
-# Archivo app.py
-
 import os
 import logging
 from logging.handlers import RotatingFileHandler
@@ -56,5 +54,3 @@ def configure_logging(app):
 
         app.logger.setLevel(logging.INFO)
         app.logger.info('Aplicación iniciada')
-
-
