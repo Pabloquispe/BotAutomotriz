0f9d3d583ca7cd2bd25e8d52823ed7a25796288b
Pabloquispe <131137809+Pabloquispe@users.noreply.github.com>
2024-07-06 15:54:36 -0500

    Update openai_config.py

--- openai_config.py
+++ openai_config.py
@@ -1,17 +1,24 @@
-# openai_config.py
 import openai
 import os
 
 # Configuración de la API de OpenAI
 openai.api_key = os.getenv('OPENAI_API_KEY')
 
-def interactuar_con_openai(pregunta):
-    response = openai.Completion.create(
-        engine="davinci",
-        prompt=pregunta,
-        max_tokens=150,
-        n=1,
-        stop=None,
-        temperature=0.9,
-    )
-    return response.choices[0].text.strip()
+def interactuar_con_openai(consulta):
+    try:
+        response = openai.ChatCompletion.create(
+            model="gpt-4",
+            messages=[
+                {"role": "system", "content": "You are a helpful assistant."},
+                {"role": "user", "content": consulta}
+            ],
+            max_tokens=150,
+            temperature=0.5,
+        )
+        return response.choices[0].message['content'].strip()
+    except openai.error.RateLimitError:
+        return "❌ **Lo siento, hemos superado nuestro límite de solicitudes por ahora. Por favor, intenta de nuevo más tarde.**"
+    except openai.error.OpenAIError as e:
+        print(f"Error interacting with OpenAI: {e}")
+        return "❌ **Ha ocurrido un error al interactuar con OpenAI. Por favor, intenta de nuevo más tarde.**"
+
