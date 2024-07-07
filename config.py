ea081e68de740c28d36f71a08853a0bd1340100b
Pabloquispe <131137809+Pabloquispe@users.noreply.github.com>
2024-07-07 07:22:47 -0500

    Update config.py

--- config.py
+++ config.py
@@ -2,13 +2,10 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    # Configuración de sesiones basada en archivos
-    SESSION_TYPE = 'filesystem'
+    SESSION_TYPE = 'redis'  # O el tipo de sesión que estás utilizando
     SESSION_PERMANENT = False
     SESSION_USE_SIGNER = True
-    SESSION_FILE_DIR = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'flask_session')
-    SESSION_FILE_THRESHOLD = 100
-    SESSION_FILE_MODE = 0o600
-    SESSION_COOKIE_NAME = 'my_session'
+    SESSION_KEY_PREFIX = 'flask_session:'
+    SESSION_REDIS = redis.from_url(os.environ.get('REDIS_URL'))
+
+
