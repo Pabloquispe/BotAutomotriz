890da43da442f6e2f804b8248df91067da00bf44
Pabloquispe <131137809+Pabloquispe@users.noreply.github.com>
2024-07-07 09:40:26 -0500

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,4 @@
 import os
-from dotenv import load_dotenv
-
-# Cargar las variables de entorno desde el archivo .env
-load_dotenv()
 
 # Directorio base de la aplicación
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
@@ -10,21 +6,18 @@ BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 class Config:
     """Configuración base utilizada para todas las configuraciones."""
     SECRET_KEY = os.environ.get('SECRET_KEY') or os.urandom(24)
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql://root@localhost/proyecto27'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-    # Configuración de sesiones basada en archivos
-    SESSION_TYPE = 'filesystem'
-    SESSION_PERMANENT = False
-    SESSION_USE_SIGNER = True
-    SESSION_FILE_DIR = os.path.join(BASE_DIR, 'flask_session')
-    SESSION_FILE_THRESHOLD = 100
-    SESSION_FILE_MODE = 0o600
-    SESSION_COOKIE_NAME = 'my_session'
+    # Configuración de horarios de servicios
+    HORARIO_INICIO_MANANA = '09:00'
+    HORARIO_FIN_MANANA = '12:00'
+    HORARIO_INICIO_TARDE = '13:00'
+    HORARIO_FIN_TARDE = '18:00'
 
 class DevelopmentConfig(Config):
     """Configuración utilizada durante el desarrollo."""
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace('mysql://', 'mysql+pymysql://')
 
 class TestingConfig(Config):
     """Configuración utilizada durante las pruebas."""
@@ -35,7 +28,6 @@ class TestingConfig(Config):
 class ProductionConfig(Config):
     """Configuración utilizada en producción."""
     DEBUG = False
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace('mysql://', 'mysql+pymysql://')
 
 # Diccionario para facilitar el acceso a las configuraciones
 config_by_name = {
