22bbaa585300583d11138d8a7f459db6ad0c27f1
Pabloquispe <131137809+Pabloquispe@users.noreply.github.com>
2024-07-06 21:50:40 -0500

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ class Config:
     SESSION_USE_SIGNER = True
     SESSION_FILE_DIR = os.path.join(BASE_DIR, 'flask_session')
     SESSION_FILE_THRESHOLD = 100
-    SESSION_FILE_MODE = 600
+    SESSION_FILE_MODE = 0o600
     SESSION_COOKIE_NAME = 'my_session'
 
 class DevelopmentConfig(Config):
