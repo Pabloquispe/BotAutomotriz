13a52e7afe0c7e8ad4552404fd841d9d1c25a913
Pabloquispe <131137809+Pabloquispe@users.noreply.github.com>
2024-07-07 07:23:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,16 @@
 from flask import Flask
 from flask_session import Session
-import os
+from config import Config
 
-def create_app():
-    app = Flask(__name__)
-    app.config.from_object('config.Config')
-    
-    # Inicializar la sesión
-    Session(app)
-    
-    @app.route('/')
-    def index():
-        return "Hello, World!"
-    
-    return app
+app = Flask(__name__)
+app.config.from_object(Config)
+
+# Inicializar la extensión Flask-Session
+Session(app)
+
+@app.route('/')
+def index():
+    return "Hello, World!"
 
 if __name__ == "__main__":
-    app = create_app()
-    app.run(debug=True)
-
+    app.run()
